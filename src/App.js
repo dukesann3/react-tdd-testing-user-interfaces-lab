8ea314281b0c61e4b812400814dc8e7ad012b09b
Duke <dukemorise33@gmail.com>
2023-10-05 18:58:15 -0700

    finished tdd testing user interface lab

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
 function App() {
-  return <div>Your code here</div>;
+  return (
+    <div>
+      <h1>Hi, I'm Dude</h1>
+      <h2>About Me</h2>
+      <img alt='this is me hoohoo' src='./sorasak-_UIN-pFfJ7c-unsplash.jpg' />
+      <p>
+        This is my Bio
+      </p>
+    </div>
+  );
 }
 
 export default App;
--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -4,3 +4,42 @@ import '@testing-library/jest-dom';
 import App from "../App";
 
 // Your tests here
+test('displays a top-level heading with the text \'Hi, I\'m Dude', () => {
+    //need to render the App component first...
+    //must render first to figure out if it has been mounted.
+    render(<App />);
+
+    //Act
+    const topLevelHeading = screen.getByRole("heading", {
+        name: /hi, I'm/i,
+        exact: false,
+        level: 1,
+    })
+
+    //Assert
+    expect(topLevelHeading).toBeInTheDocument();
+});
+
+test('displays an image of yourself with alt text identifying the content of the image', () => {
+    //ok, render App...
+    render(<App />);
+    const getImage = screen.getByAltText(/this is me hoohoo/i);
+    expect(getImage).toBeInTheDocument();
+});
+
+test('displays a second-level heading with the text About Me', () => {
+    render(<App />);
+    const aboutMeSecondLevel = screen.getByRole('heading', {
+        name: /About Me/i,
+        level: 2,
+        exact: false
+    });
+    expect(aboutMeSecondLevel).toBeInTheDocument();
+});
+
+test('displays a paragraph for your biography', () => {
+    render(<App />);
+    const paragraphWorkAround = document.querySelector('p')
+    expect(paragraphWorkAround).toBeInTheDocument();
+})
+
